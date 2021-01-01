40b86ab0f22680c704197d4a1c8850bc99d79552
jonny <ga86lad@mytum.de>
2021-01-01 10:20:27 +0100

    auto open  timers on trigger

--- server/angular/smarthome/src/app/settings/sender-bottom-sheet/sender-bottom-sheet.component.ts
+++ server/angular/smarthome/src/app/settings/sender-bottom-sheet/sender-bottom-sheet.component.ts
@@ -10,68 +10,71 @@ import { EventsComponent } from './events/events.component';
 import { TimersComponent } from './timers/timers.component';
 
 @Component({
-  selector: 'app-sender-bottom-sheet',
-  templateUrl: './sender-bottom-sheet.component.html',
-  styleUrls: ['./sender-bottom-sheet.component.less']
+	selector: 'app-sender-bottom-sheet',
+	templateUrl: './sender-bottom-sheet.component.html',
+	styleUrls: ['./sender-bottom-sheet.component.less']
 })
 export class SenderBottomSheetComponent implements OnInit {
 
 
-  public transformer: TransformFe = {}
-  title$: Observable<string>;
-  constructor(@Inject(MAT_SNACK_BAR_DATA) public data: Sender,
-    private service: SettingsService,
-    private snackbarRef: MatSnackBarRef<any>, private dialog: MatDialog,
-    private cdr: ChangeDetectorRef) {
-    this.transformer = this.data.transformation[0] || {}
+	public transformer: TransformFe = {}
+	title$: Observable<string>;
+	constructor(@Inject(MAT_SNACK_BAR_DATA) public data: Sender,
+		private service: SettingsService,
+		private snackbarRef: MatSnackBarRef<any>, private dialog: MatDialog,
+		private cdr: ChangeDetectorRef) {
+		this.transformer = this.data.transformation[0] || {}
 
-    this.title$ = this.service.getSenderTitleKeys(this.data.id);
-  }
+		this.title$ = this.service.getSenderTitleKeys(this.data.id);
+	}
 
-  isManual() {
-    return this.data.type == "manual"
-  }
+	isManual() {
+		return this.data.type == "manual"
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 
-  displayTimers() {
-    this.dialog.open(TimersComponent, {
-      data: this.data,
-      panelClass: 'unlimitedsnackbar',
-    });
-  }
-  displayBattery() {
-    this.dialog.open(BatteryComponent, {
-      data: this.data,
-      panelClass: 'unlimitedsnackbar',
-    });
-  }
+	displayTimers() {
+		this.dialog.open(TimersComponent, {
+			data: this.data,
+			panelClass: 'unlimitedsnackbar',
+		});
+	}
+	displayBattery() {
+		this.dialog.open(BatteryComponent, {
+			data: this.data,
+			panelClass: 'unlimitedsnackbar',
+		});
+	}
 
-  displayEvents() {
-    this.dialog.open(EventsComponent, {
-      data: this.data,
-      panelClass: 'unlimitedsnackbar',
-    });
-  }
+	displayEvents() {
+		this.dialog.open(EventsComponent, {
+			data: this.data,
+			panelClass: 'unlimitedsnackbar',
+		});
+	}
 
-  async deleteSender() {
-    await this.service.deleteSender(this.data.id).toPromise();
-    this.snackbarRef.dismissWithAction()
-  }
-  async send() {
-    const dataObj: { [key: string]: any } = {
-      deviceKey: this.data.deviceKey
-    };
-    if (!this.isManual()) {
-      dataObj.testsend = true
-    }
-    if (this.data.transformationAttribute && this.transformer) {
-      dataObj[this.data.transformationAttribute] = this.transformer.transformationKey
-    }
+	async deleteSender() {
+		await this.service.deleteSender(this.data.id).toPromise();
+		this.snackbarRef.dismissWithAction()
+	}
+	async send() {
+		const dataObj: { [key: string]: any } = {
+			deviceKey: this.data.deviceKey
+		};
+		if (!this.isManual()) {
+			dataObj.testsend = true
+		}
+		if (this.data.transformationAttribute && this.transformer) {
+			dataObj[this.data.transformationAttribute] = this.transformer.transformationKey
+		}
 
-    await this.service.send(dataObj).toPromise();
-    this.displayTimers();
-  }
+		await this.service.send(dataObj).toPromise();
+		if (this.transformer && this.transformer.tsTransformation.includes("delay")
+			&& this.transformer.tsTransformation.includes("promise")) {
+			this.displayTimers();
+		}
+	}
 }
