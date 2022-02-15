ff06cb34b38bc12f181bcf26478d23b2ef084c4e
jonnytest1 <jonathanheindl@gmx.de>
2022-02-15 19:08:28 +0100

    laod stpred tempaltes from server

---
+++ server/src/resources/wiring.r.ts
@@ -0,0 +1,40 @@
+
+
+
+import { template } from '@babel/core'
+import { GET, HttpRequest, HttpResponse, Path } from 'express-hibernate-wrapper'
+import { readdir, readFile } from "fs/promises"
+import { join } from "path"
+
+const bakcendRootFolder = join(__dirname, "../..")
+const gitRootFodler = join(bakcendRootFolder, "..")
+
+
+async function findTemplates(dir = gitRootFodler) {
+    const entries = await readdir(dir, { withFileTypes: true });
+    const entryEntries: Array<Array<{ name: string, content: string }>> = await Promise.all(entries.map(async entry => {
+        const next = join(dir, entry.name)
+        if (entry.isDirectory() && !next.endsWith("node_modules") && !next.endsWith(".angular") && !next.endsWith("__pycache__")) {
+            return findTemplates(next)
+        } else if (entry.name.endsWith(".template")) {
+            return [{
+                name: next,
+                content: await readFile(next, { encoding: "utf8" })
+            }]
+        }
+        return []
+    }))
+
+    return entryEntries.flat()
+
+}
+
+const templates = findTemplates();
+@Path("wiring")
+export class TransformationResource {
+    @GET("")
+    async getTemplates(req: HttpRequest, res: HttpResponse) {
+        const templateList = await templates
+        res.send(templateList)
+    }
+}
