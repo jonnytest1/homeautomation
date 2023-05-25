9d699b9c2347e1acdc916972108bcae29b304ec1
jonnytest1 <jonathanheindl@gmx.de>
2023-05-25 15:18:21 +0200

    lamp update

--- clients/arduino/lamp/lamp/src/lib/encoding.h
+++
@@ -1,5 +0,0 @@
-#include "arduinoref.h"
-#include <map>
-
-String base64_encode(const String &in);
-String json(const std::map<String, String> content);
--- clients/arduino/lamp/lamp/src/main.cpp
+++ clients/arduino/lamp/lamp/src/main.cpp
@@ -1,36 +1,80 @@
 #include <Arduino.h>
 #include <HTTPClient.h>
 #include "lib/http.h"
-#include <WiFi.h>
 #include "lib/log.h"
 #include "lib/uuid.h"
+#include "lib/prop.h"
+#include "lib/str.h"
+#include "lib/jsonnode.h"
+#include "lib/http_client.h"
+#include "lib/wi_fi.h"
 #include "ArduinoOTA.h"
 
-String deviceKey = "lamp-component";
 int pinState = LOW;
 #define PORT 80
 
-String onRequest(HttpRequest *client)
+int lampOn = LOW;
+int lampOff = HIGH;
+
+bool receiverCreate = false;
+void triggerHandler(int code, std::string data)
 {
-    pinState = pinState == LOW ? HIGH : LOW;
-    digitalWrite(12, pinState);
-
-    return "currently " + String(pinState);
-}
-
-HttpServer server(PORT, onRequest);
-
-void triggerHandler(int code, String data)
-{
+    Serial.println("triggerh");
     if (code != HTTP_CODE_OK && code != 409)
     {
-        Serial.println(data);
-        logData("ERROR", "error in request", {{"application", deviceKey}, {"code", String(code)}, {"error", data}});
+        Serial.println(data.c_str());
+        logData("ERROR", "error in request", {{"application", getDeviceKey().c_str()}, {"code", String(code).c_str()}, {"error", data.c_str()}});
     }
 }
+String onRequest(HttpRequest *client)
+{
+
+    Serial.println("request");
+    if (client->path.indexOf("action") > -1)
+    {
+        Serial.println(client->body);
+        JsonNode body = parseJson(client->body);
+        Serial.println("parsing done");
+        JsonNode name = body.objectContent.at("name");
+
+        Serial.println("got name");
+        if (name.textValue == "on")
+        {
+            Serial.println("on");
+            digitalWrite(12, lampOn);
+            pinState = lampOn;
+        }
+        else if (name.textValue == "off")
+        {
+            Serial.println("off");
+            digitalWrite(12, lampOff);
+            pinState = lampOff;
+        }
+
+        HttpClientRequest request(serverEndpoint() + "/nodets/rest/receiver/state");
+        request.jsonBody(stringify(JsonFactory::obj({
+            {"deviceKey", JsonFactory::str(getDeviceKey().c_str())},
+            {"state", JsonFactory::str(pinState == lampOn ? "true" : "false")},
+        })));
+        request.callback = triggerHandler;
+        request.send();
+
+        return "currently " +
+               String(pinState);
+    }
+    else
+    {
+        pinState = pinState == LOW ? HIGH : LOW;
+        digitalWrite(12, pinState);
+
+        return "currently " + String(pinState);
+    }
+}
+HttpServer server(PORT, onRequest);
 
 void setup()
 {
+
     delay(1000);
     // put your setup code here, to run once:
     Serial.begin(9600);
@@ -38,33 +82,69 @@ void setup()
     pinMode(12, OUTPUT);
     digitalWrite(12, pinState);
 
-    request("https://192.168.178.54/nodets/rest/receiver", {
-                                                               {"deviceKey", deviceKey.c_str()},
-                                                               {"port", String(PORT).c_str()},
-                                                               {"type", "http"},
-                                                               {"name", "lamp"},
-                                                               {"description", "toggle lamp on and off"},
-                                                           },
-            triggerHandler, false);
-    Serial.println("");
-    Serial.println("WiFi connected.");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-    String otaPassword = generateUuid();
-    logData("INFO", " startup log", {
-                                        {"application", deviceKey},
-                                        {"otaPassword", otaPassword},
-                                        {"ip", server.getIp()},
-                                    });
+    std::string otaPassword = generateUuid();
+    Serial.println(otaPassword.c_str());
+
     server.begin();
 
-    ArduinoOTA.setHostname(("esp32-" + deviceKey + "_ota").c_str());
+    Serial.println("");
+    Serial.println("IP address: ");
+    Serial.println(getDeviceIp().c_str());
+
+    ArduinoOTA.setHostname(("esp32-" + getDeviceKey() + "_ota").c_str());
     ArduinoOTA.setPassword(otaPassword.c_str());
     ArduinoOTA.begin();
+    Serial.println("ota set up");
+    logData("INFO", "startup log", {
+                                       {"application", getDeviceKey().c_str()}, //
+                                       {"otaPassword", otaPassword},
+                                       {"ip", getDeviceIp().c_str()},
+                                   });
+    Serial.println("logged v1");
+}
+
+void registerReceiver()
+{
+    Serial.println("registering receiver");
+    JsonNode actions = JsonFactory::list( //
+        {                                 //
+         JsonFactory::obj(                //
+             {
+                 //
+                 {"name", JsonFactory::str("off")},
+             }),
+         JsonFactory::obj( //
+             {
+                 //
+                 {"name", JsonFactory::str("on")},
+             })
+
+        });
+    JsonNode data = JsonFactory::obj({
+        {"deviceKey", JsonFactory::str(getDeviceKey().c_str())},
+        {"port", String(PORT).c_str()},
+        {"type", JsonFactory::str("http")},
+        {"name", JsonFactory::str("lamp")},
+        {"state", JsonFactory::str("boolean")},
+        {"description", JsonFactory::str("toggle lamp on and off")},
+        {"actions", actions} //
+    });
+
+    HttpClientRequest request(serverEndpoint() + "/nodets/rest/receiver");
+    request.jsonBody(stringify(data));
+    request.callback = triggerHandler;
+    Serial.println("sending reuqest");
+    request.send();
 }
 
 void loop()
 {
     server.step();
     ArduinoOTA.handle();
+
+    if (!receiverCreate)
+    {
+        registerReceiver();
+        receiverCreate = true;
+    }
 }
