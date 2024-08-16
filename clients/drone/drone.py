aa16a6c006cf7546f2e2f2e8c57034f8deb79dcf
jonnytest1 <jonathanheindl@gmx.de>
2024-08-16 16:31:38 +0200

    windows wifi control

--- clients/drone/drone.py
+++ clients/drone/drone.py
@@ -1,31 +1,99 @@
+import datetime
+from time import sleep
+from typing import Any, Union
 from djitellopy import Tello
 # https://github.com/damiafuentes/DJITelloPy/blob/master/README.md
-from cv2 import imshow
-from cv2.typing import MatLike
-from wifi import connect_to_network, get_networks
 
-# while (True):
-#    networks = get_networks()
+from wifi import NetworkItem, connect_to_network, get_networks, get_status
+from cv2 import imshow, imwrite
 
-#   for netowrk in networks:
-#       if netowrk.startswith("TELLO"):
-#          connect_to_network(netowrk)
-#          break
-#
-# subprocess.check_output(
-#   "netsh wlan connect ssid=YOUR-WIFI-SSID name=PROFILE-NAME")
+connected = False
+
+
+tello_Id = "TELLO-E9"
+
+status = get_status()
+for interface_status in status:
+    if interface_status["Status"] != "getrennt" and tello_Id in interface_status["SSID"]:
+        connected = True
+        print("already connected")
+
+while (not connected):
+    networks = get_networks()
+
+    tellos: dict[str, list[NetworkItem]] = {}
+
+    for network_interface in networks:
+        for network in network_interface.get_networks():
+            if network.ssid.startswith(tello_Id):
+                print("got tello wifi")
+                if network.ssid not in tellos:
+                    tellos[network.ssid] = []
+                tellos[network.ssid].append(network)
+
+    for item in tellos.values():
+        strongest: Union[NetworkItem, None] = None
+        strength = -1
+        for interface_con in item:
+            if interface_con.max_strength > strength:
+                strongest = interface_con
+                strength = interface_con.max_strength
+
+        if strongest is not None:
+            print(
+                f"connecting to {strongest.ssid} with {strongest.max_strength}%")
+            connect_to_network(strongest)
+            connected = True
+    if connected:
+        break
+    # subprocess.check_output(
+    #  "netsh wlan connect ssid=YOUR-WIFI-SSID name=PROFILE-NAME")
 
 
 tello = Tello()
-tello.connect()
 
-print(tello.get_battery())
+
+def connnect():
+    e1: Union[Exception, None] = None
+    for i in range(5):
+        try:
+            tello.connect()
+            return
+        except Exception as e:
+            e1 = e
+            print(e)
+    if e1 is not None:
+        raise e1
+    raise Exception("no connection")
+
+
+connnect()
+
+print(f"battery: {tello.get_battery()}")
 
 tello.streamon()
+sleep(0.5)
 
-img = tello.get_frame_read().frame
-if img is not None:
-    imshow("img", img)
+frameread: Any
+for i in range(10):
+    try:
+        frameread = tello.get_frame_read()
+        break
+    except Exception as e:
+        if i == 8:
+            raise e
+        print(e)
+
+while True:
+    try:
+        img = frameread.frame
+        if img is not None:
+            now = datetime.datetime.now().timestamp()
+            imwrite(f"picture{now}.png", img)
+
+    except Exception as e:
+        print("err", e)
+        pass
 # tello.connect()
 # tello.takeoff()
 # tello.move_up(100)
--- clients/drone/wifi.py
+++ clients/drone/wifi.py
@@ -1,21 +1,153 @@
+import os
 import subprocess
-from re import findall, DOTALL
+from typing import Union
+from util.netshparser import parse_to_json
+from json import loads, dumps
+from os.path import join, dirname
+
+'''
+
+if its just one network card connected it will just be the nested object not an array
+[
+{
+  Schnittstellenname : WLAN
+  SSID 1 : {
+    name:str
+    (...)
+    BSSID 1:{
+      Signal: '18%'
+      name: mac
+      (...)
+    }
+    BSSID 2 ...
+  }
+  SSID 2 ...
+}
+...
+}
+'''
+
+
+class NetworkItem:
+    def __init__(self, data: dict, interface_ref: "NetworkInterface"):
+        self.ssid: str = data["name"]
+        self.interface = interface_ref
+
+        self.max_strength = -1
+
+        for key, bssi in data.items():
+            key_str: str = key
+            if key_str.startswith("BSSID"):
+                signal = int(bssi["Signal"].replace("%", ""))
+                if signal > self.max_strength:
+                    self.max_strength = signal
+
+
+class NetworkInterface:
+
+    def __init__(self, data: dict):
+        self.data = data
+        self.interace_name: str = data["Schnittstellenname"]
+
+        self.networks = []
+
+        for key, value in self.data.items():
+            self.networks.append(value)
+
+    def get_networks(self):
+        return [NetworkItem(n, self) for n in self.networks if isinstance(n, dict)]
 
 
 def get_networks():
     process = subprocess.run(
-        ['netsh', 'wlan', 'show', 'network'], capture_output=True)
+        ['netsh', 'wlan', 'show', 'network', 'mode=bssid'], capture_output=True)
     process.check_returncode()
     devicestr = process.stdout.decode("cp850")
-    devicestr = devicestr.replace("\r", "")
+    results = parse_to_json(devicestr)
 
-    result: list[str] = findall(
-        r"SSID \d+ : ([^ ]*)\n", devicestr, DOTALL)
-    return result
+    if isinstance(results, dict):
+        results = [results]
+
+    return [NetworkInterface(r) for r in results]
 
 
-def connect_to_network(network: str):
-    # netsh wlan show interface
+def create_profile(net: NetworkItem, name: str = ""):
+
+    profile_hex = ''.join(format(ord(char), 'X') for char in net.ssid)
+    pwd = ""
+    if name == "":
+        name = net.ssid
+
+    profile_xml = f"""<?xml version="1.0"?>
+<WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1">
+    <name>{name}</name>
+    <SSIDConfig>
+        <SSID>
+            <hex>{profile_hex}</hex>
+            <name>{net.ssid}</name>
+        </SSID>
+    </SSIDConfig>
+    <connectionType>ESS</connectionType>
+    <connectionMode>auto</connectionMode>
+    <MSM>
+        <security>
+            <authEncryption>
+                <authentication>open</authentication>
+                <encryption>none</encryption>
+                <useOneX>false</useOneX>
+            </authEncryption>
+        </security>
+    </MSM>
+</WLANProfile>
+    """
+
+    file_target = join(dirname(__file__), "profile.xml")
+    with open(file_target, "w") as file:
+        file.write(profile_xml)
+
+    #
+    cmd_safe_path = file_target
     process = subprocess.run(
-        ['netsh', 'wlan', 'connect', f'ssid={network}', f"name=dfdfgdf24"], capture_output=True)
+        ['netsh', 'wlan', 'add', "profile", f"filename=\"{cmd_safe_path}\"", f"interface=\"{net.interface.interace_name}\""], capture_output=True)
+
+    stdout = process.stdout.decode("cp850")
+    print(stdout)
+    process.check_returncode()
+
+
+def get_status():
+
+    process = subprocess.run(
+        ['netsh', 'wlan', 'show', "interfaces"], capture_output=True)
+
+    stdout = process.stdout.decode("cp850")
+    results = parse_to_json(stdout)
+    if (isinstance(results, list)):
+        raise Exception("unexpted type")
+    return [next(iter(results.values()))]
+
+
+def connect_to_network(network: NetworkItem):
+
+    process = subprocess.run(
+        ['netsh', 'wlan', 'show', "profile"], capture_output=True)
+    process.check_returncode()
+
+    decoded = process.stdout.decode("cp850")
+
+    profilename = network.ssid
+
+    if f": {profilename}" not in decoded:
+        print("creating profile")
+        create_profile(network, profilename)
+
+    # TELLO-E96178
+    # netsh wlan show interface
+    # netsh wlan connect ssid=TELLO-E96178 name=TELLO-E96178
+    process = subprocess.run(
+        f'netsh wlan connect name="{profilename}" interface="{network.interface.interace_name}"',
+        capture_output=True, shell=True)
+
+    decoded = process.stdout.decode("cp850")
+    print(decoded)
     process.check_returncode()
