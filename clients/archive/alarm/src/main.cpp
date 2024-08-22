cb912c343dd05ab534b78da7f52ef4cf2f801786
jonnytest1 <jonathanheindl@gmx.de>
2024-08-22 17:51:04 +0200

    removed hardcoded secret for archived project

--- clients/archive/alarm/src/main.cpp
+++ clients/archive/alarm/src/main.cpp
@@ -17,78 +17,78 @@ int motorPin = 16;
 
 void onAction(std::string action, SmartHome *smarthomeRef)
 {
-    if (action == "dismiss")
-    {
-        digitalWrite(motorPin, LOW);
-        smarthomeRef->updateState(JsonFactory::FALSE());
-    }
-    else if (action == "enable")
-    {
-        setPinHighTimed(motorPin, 1000 * 10);
-    }
+  if (action == "dismiss")
+  {
+    digitalWrite(motorPin, LOW);
+    smarthomeRef->updateState(JsonFactory::FALSE());
+  }
+  else if (action == "enable")
+  {
+    setPinHighTimed(motorPin, 1000 * 10);
+  }
 }
 
 std::string onRequest(HttpRequest *request)
 {
-    std::string response = "";
-    // response += request->path;
-    // response += request->body + "headers: " + json(request->headers) + " hraw:" + request->requestHeader;
-    if (request->path.startsWith("/healthcheck"))
+  std::string response = "";
+  // response += request->path;
+  // response += request->body + "headers: " + json(request->headers) + " hraw:" + request->requestHeader;
+  if (request->path.startsWith("/healthcheck"))
+  {
+    response = "ok";
+  }
+  else if (request->path.startsWith("/alarm"))
+  {
+    Serial.println("alarm");
+    if (request->headers.count("content-type") == 1)
     {
-        response = "ok";
-    }
-    else if (request->path.startsWith("/alarm"))
-    {
-        Serial.println("alarm");
-        if (request->headers.count("content-type") == 1)
+      if (request->headers.at("content-type") == "application/json")
+      {
+        Serial.println("json");
+        Serial.println(request->body);
+        // response += "json";
+        JsonNode jsonBody = parseJson(request->body);
+        if (jsonBody.isObject)
         {
-            if (request->headers.at("content-type") == "application/json")
+          JsonNode eventNode = jsonBody.objectContent.at("event");
+          JsonNode nameNode = jsonBody.objectContent.at("value2");
+
+          std::map<std::string, std::string> logDataMap = {
+              {"application", getDeviceKey()},
+              {"event", eventNode.textValue},
+              {"alarm_time", jsonBody.objectContent.at("value1").textValue},
+              {"alarm_name", nameNode.textValue}};
+
+          logData("INFO", "alarm event", logDataMap);
+          if (eventNode.isText && eventNode.textValue == "alarm_alert_start")
+          {
+            Serial.println("alrm start");
+            response += "alarmstart\n";
+
+            if (nameNode.isText && str_includes(nameNode.textValue, "motor"))
             {
-                Serial.println("json");
-                Serial.println(request->body);
-                // response += "json";
-                JsonNode jsonBody = parseJson(request->body);
-                if (jsonBody.isObject)
-                {
-                    JsonNode eventNode = jsonBody.objectContent.at("event");
-                    JsonNode nameNode = jsonBody.objectContent.at("value2");
-
-                    std::map<std::string, std::string> logDataMap = {
-                        {"application", getDeviceKey()},
-                        {"event", eventNode.textValue},
-                        {"alarm_time", jsonBody.objectContent.at("value1").textValue},
-                        {"alarm_name", nameNode.textValue}};
-
-                    logData("INFO", "alarm event", logDataMap);
-                    if (eventNode.isText && eventNode.textValue == "alarm_alert_start")
-                    {
-                        Serial.println("alrm start");
-                        response += "alarmstart\n";
-
-                        if (nameNode.isText && str_includes(nameNode.textValue, "motor"))
-                        {
-                            Serial.println("enable motor");
-                            response += "motor\n";
-                            setPinHighTimed(motorPin, 1000 * 10);
-                        }
-                    }
-                }
+              Serial.println("enable motor");
+              response += "motor\n";
+              setPinHighTimed(motorPin, 1000 * 10);
             }
+          }
         }
+      }
     }
-    else if (request->path.startsWith("/close"))
-    {
-        digitalWrite(motorPin, LOW);
-        response = "closed";
-    }
-    else
-    {
-        response += request->body.c_str();
-        response += " hraw:";
-        response += request->requestHeader.c_str();
-        request->sendHeader(501, response.length());
-    }
-    return response;
+  }
+  else if (request->path.startsWith("/close"))
+  {
+    digitalWrite(motorPin, LOW);
+    response = "closed";
+  }
+  else
+  {
+    response += request->body.c_str();
+    response += " hraw:";
+    response += request->requestHeader.c_str();
+    request->sendHeader(501, response.length());
+  }
+  return response;
 }
 
 SmartHome smarthome(onAction);
@@ -96,52 +96,52 @@ SmartHome smarthome(onAction);
 JsonNode getReceiver()
 {
 
-    JsonNode actions = JsonFactory::list( //
-        {                                 //
-         JsonFactory::obj(                //
-             {
-                 //
-                 {"name", JsonFactory::str("enable")},
-                 {"confirm", JsonFactory::TRUE()},
-             }),
-         JsonFactory::obj( //
-             {
-                 //
-                 {"name", JsonFactory::str("dismiss")},
-             })
+  JsonNode actions = JsonFactory::list( //
+      {                                 //
+       JsonFactory::obj(                //
+           {
+               //
+               {"name", JsonFactory::str("enable")},
+               {"confirm", JsonFactory::TRUE()},
+           }),
+       JsonFactory::obj( //
+           {
+               //
+               {"name", JsonFactory::str("dismiss")},
+           })
 
-        });
-    return JsonFactory::obj({
-        {"name", JsonFactory::str("alarm motor")},
-        {"state", JsonFactory::str("boolean")},
-        {"description", JsonFactory::str("motor for to rattle the bed until i wake up")},
-        {"actions", actions} //
-    });
+      });
+  return JsonFactory::obj({
+      {"name", JsonFactory::str("alarm motor")},
+      {"state", JsonFactory::str("boolean")},
+      {"description", JsonFactory::str("motor for to rattle the bed until i wake up")},
+      {"actions", actions} //
+  });
 }
 
 void setup()
 {
-    Serial.begin(115200);
-    Serial.println("start");
+  Serial.begin(115200);
+  Serial.println("start");
 
-    pinMode(motorPin, OUTPUT);
+  pinMode(motorPin, OUTPUT);
 
-    std::string otaPassword = "fskz570s-36inwky3-m8y4kg28-wh3xzzaf"; // generateUuid();
-    Serial.println("ota password: " + String(otaPassword.c_str()));
-    smarthome.fallbackRequestHandler = onRequest;
-    smarthome.getReceiverConfig = getReceiver;
-    smarthome.init();
+  std::string otaPassword = generateUuid();
+  Serial.println("ota password: " + String(otaPassword.c_str()));
+  smarthome.fallbackRequestHandler = onRequest;
+  smarthome.getReceiverConfig = getReceiver;
+  smarthome.init();
 
-    logData("INFO", "startup log", {{"otaPassword", otaPassword}, {"ip", getDeviceIp()}});
+  logData("INFO", "startup log", {{"otaPassword", otaPassword}, {"ip", getDeviceIp()}});
 
-    ArduinoOTA.setHostname(("esp32-" + getDeviceKey() + "_ota").c_str());
-    ArduinoOTA.setPassword(otaPassword.c_str());
-    ArduinoOTA.begin();
-    Serial.println("v1");
+  ArduinoOTA.setHostname(("esp32-" + getDeviceKey() + "_ota").c_str());
+  ArduinoOTA.setPassword(otaPassword.c_str());
+  ArduinoOTA.begin();
+  Serial.println("v1");
 }
 
 void loop()
 {
-    smarthome.step();
-    ArduinoOTA.handle();
+  smarthome.step();
+  ArduinoOTA.handle();
 }
