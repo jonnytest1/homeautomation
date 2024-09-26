688ea3cfd38c06f24568abfd87dbed2f65f799ba
jonnytest1 <jonathanheindl@gmx.de>
2024-09-26 22:51:00 +0200

    connect with setup

--- .gitea/workflows/deploy.py
+++ .gitea/workflows/deploy.py
@@ -1,6 +1,18 @@
 import socket
 import os
 
+import debugpy
+
+try:
+    debugpy.listen(("0.0.0.0", 5678))
+    print("Waiting for debugger attach 5678")
+except:
+    debugpy.listen(("0.0.0.0", 5679))
+    print("Waiting for debugger attach on fallback port 5679")
+
+debugpy.wait_for_client()
+
+
 socket_path = '/var/run/c_dpl.sock'
 
 client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
