72bb7715391cee9f94272952fcd944f3dcfce75b
jonnytest1 <jonathanheindl@gmx.de>
2024-09-26 19:26:44 +0200

    connect to socket

--- .gitea/workflows/deploy.py
+++ .gitea/workflows/deploy.py
@@ -1,4 +1,17 @@
-import sys
+import socket
+import os
 
-print("deploy.py start ")
-print(sys.argv)
+socket_path = '/home/smarthome/deployment/dpl.sock'
+
+client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+
+client.connect(socket_path)
+
+message = 'Hello from the client!'
+client.sendall(message.encode())
+
+response = client.recv(1024)
+print(f'Received response: {response.decode()}')
+
+client.close()
