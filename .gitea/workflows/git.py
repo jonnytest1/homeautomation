8d0a14009709d66168cc03d46d0a7c217486bdb1
jonnytest1 <jonathanheindl@gmx.de>
2024-10-05 17:00:23 +0200

    strip empty lines

--- .gitea/workflows/git.py
+++ .gitea/workflows/git.py
@@ -7,4 +7,4 @@ def get_changed_files():
     process = subprocess.run(
         "git diff --name-only HEAD HEAD~1", shell=True, stdout=subprocess.PIPE)
 
-    return process.stdout.decode().split('\n')
+    return process.stdout.decode().strip().split('\n')
