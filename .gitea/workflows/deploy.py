ad06591576311995081ce1a878d539f6ec87f301
jonnytest1 <jonathanheindl@gmx.de>
2024-10-05 18:52:57 +0200

    read changes from last action

--- .gitea/workflows/deploy.py
+++ .gitea/workflows/deploy.py
@@ -2,7 +2,7 @@ import socket
 import os
 import json
 
-from git import get_changed_files
+from git import current_commit, get_changed_files
 
 socket_path = '/var/run/dpl/c_dpl.sock'
 
@@ -16,7 +16,16 @@ client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 print(f"connecting to {socket_path}")
 client.connect(socket_path)
 
-changed_files = get_changed_files()
+
+diff_since = "HEAD~1"
+commit_file = "/var/run/dpl/last_action/commit"
+if os.path.exists(path=commit_file):
+    f = open(commit_file, "r")
+    diff_since = f.read()
+    f.close()
+
+print("diffing from "+diff_since)
+changed_files = get_changed_files(diff_since)
 print("git diff: ")
 print(changed_files)
 message = json.dumps(dict(type="deploy", files=changed_files))
@@ -35,6 +44,10 @@ while True:
             print(line)
 
         client.close()
+
+        file = open(commit_file, "w")
+        file.write(current_commit())
+        file.close()
         break
     except json.JSONDecodeError as e:
         continue
--- .gitea/workflows/git.py
+++ .gitea/workflows/git.py
@@ -2,9 +2,16 @@
 import subprocess
 
 
-def get_changed_files():
-    print(subprocess.run("pwd", shell=True).stdout)
+def get_changed_files(to_parent: str):
+    print(subprocess.run(args="pwd", shell=True).stdout)
     process = subprocess.run(
-        "git diff --name-only HEAD HEAD~1", shell=True, stdout=subprocess.PIPE)
+        f"git diff --name-only HEAD {to_parent}", shell=True, stdout=subprocess.PIPE)
 
     return process.stdout.decode().strip().split('\n')
+
+
+def current_commit():
+    process = subprocess.run(
+        "git rev-parse HEAD", shell=True, stdout=subprocess.PIPE)
+
+    return process.stdout.decode().strip()
