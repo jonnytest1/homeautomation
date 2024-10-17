772cddd14bc4fd22d239adf0ebd125ee7718ef5d
jonnytest1 <jonathanheindl@gmx.de>
2024-10-17 14:46:41 +0200

    restore old nodes

--- server/src/services/generic-node/generic-node-data-loader.ts
+++ server/src/services/generic-node/generic-node-data-loader.ts
@@ -4,9 +4,12 @@ import { genericNodeDataStore } from './generic-store/reference';
 import type { NodeData } from './typing/generic-node-type';
 import { NodeContextData } from './models/node-backup';
 import { NodeEntry } from './models/node-entry';
+import { nodesDataFolder } from './generic-node-constants';
+import type { ElementNode } from './typing/element-node';
 import { logKibana } from '../../util/log';
-import { load, PsqlBase, type DataBaseBase } from 'hibernatets';
-
+import { load, PsqlBase, save, type DataBaseBase } from 'hibernatets';
+import { readdirSync, readFileSync } from "fs"
+import { join } from 'path';
 
 
 const backupPool = new PsqlBase({
@@ -56,7 +59,40 @@ async function loadNodeFiles() {
         db: backupPool
       }
     })
-    return nodes.map(n => n.node)
+    const nodeList = nodes.map(n => n.node)
+
+    const nodeIds = new Set(nodes.map(n => n.nodeUuid))
+
+    const nodeEntries = readdirSync(nodesDataFolder, { withFileTypes: true });
+
+
+
+    for (const nodeEntry of nodeEntries) {
+      if (nodeEntry.isFile()) {
+        const file = join(nodesDataFolder, nodeEntry.name)
+
+        if (!nodeIds.has(nodeEntry.name.split(".json")[0])) {
+          const nodeFile = readFileSync(file, { encoding: "utf8" })
+          const nodeData = JSON.parse(nodeFile) as ElementNode
+
+          save(NodeEntry.from(nodeData), { db: backupPool, updateOnDuplicate: true })
+          nodeList.push(nodeData)
+        }
+
+
+
+
+
+
+
+
+
+      }
+    }
+
+
+
+    return nodeList
 
 
   } catch (e) {
