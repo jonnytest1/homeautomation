4d94e252eb1dd102d8a475002dbf3ac1d620cda1
jonnytest1 <jonathanheindl@gmx.de>
2024-10-28 12:05:01 +0100

    only run abort once

--- clients/pc-receiver/util/abortable.ts
+++ clients/pc-receiver/util/abortable.ts
@@ -19,12 +19,15 @@ interface AbortOpts {
 
 export async function abortable(calls: Array<() => Promise<unknown>>, opts: AbortOpts = {}) {
   const start = Date.now()
-
+  let doneAbortAttempt = false
   for (const call of calls) {
     await call();
     if (abortTs > start) {
       if (opts.abortDisabled) {
-        await opts.onAbortAttempt?.()
+        if (!doneAbortAttempt) {
+          await opts.onAbortAttempt?.()
+          doneAbortAttempt = true
+        }
       } else {
         console.log(`calling abort`)
         await opts.onAbort?.()
