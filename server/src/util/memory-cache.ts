63aeaef15df68b785ad8e90a36f6d1308b89eddf
jonnytest1 <jonathanheindl@gmx.de>
2025-04-13 18:39:49 +0200

    use weakref for cache

--- server/src/util/memory-cache.ts
+++ server/src/util/memory-cache.ts
@@ -1,31 +1,32 @@
 
-interface MemoryCacheOptions{
-  key:string
+interface MemoryCacheOptions {
+  key: string
 
-  validityMillis?:number
+  validityMillis?: number
 }
 
 
-const cacheObj:Record<string,{
-  obj:unknown,
-  generated:number
-}> ={}
+const cacheObj: Record<string, WeakRef<{
+  obj: unknown,
+  generated: number
+}>> = {}
 
 
-export async function memoryCache<T>(options:MemoryCacheOptions|string,generator:(key:string)=>(T|Promise<T>)):Promise<T>{
-  if(typeof options=="string"){
-    options={
-      key:options
+export async function memoryCache<T>(options: MemoryCacheOptions | string, generator: (key: string) => (T | Promise<T>)): Promise<T> {
+  if (typeof options == "string") {
+    options = {
+      key: options
     }
   }
-  let cacheInstance=cacheObj[options.key]
-  const validityMillis=options.validityMillis??1000*60;
-  if(!cacheInstance||(cacheInstance.generated+validityMillis)<Date.now()){
-    cacheInstance={
-      obj:await generator(options.key),
-      generated:Date.now()
+  let cacheInstance = cacheObj[options.key]?.deref()
+  const validityMillis = options.validityMillis ?? 1000 * 60;
+
+  if (!cacheInstance || (cacheInstance.generated + validityMillis) < Date.now()) {
+    cacheInstance = {
+      obj: await generator(options.key),
+      generated: Date.now()
     }
-    cacheObj[options.key]=cacheInstance
+    cacheObj[options.key] = new WeakRef(cacheInstance)
   }
   return cacheInstance.obj as T
 }
