32e43fcfb8bb550b82df55405c6d3b85adc29308
jonnytest1 <jonathanheindl@gmx.de>
2025-06-21 10:37:36 +0200

    add handling for earlier difs

--- .gitea/workflows/git.py
+++ .gitea/workflows/git.py
@@ -2,12 +2,24 @@
 import subprocess
 
 
+def increase_fetch_ct():
+    print(subprocess.run(args="pwd", shell=True).stdout)
+    process = subprocess.run("git fetch --depth=100", shell=True, stdout=subprocess.PIPE)
+
+    return process.stdout.decode().strip().split('\n')
+
+
 def get_changed_files(to_parent: str):
     print(subprocess.run(args="pwd", shell=True).stdout)
     process = subprocess.run(
         f"git diff --name-only HEAD {to_parent}", shell=True, stdout=subprocess.PIPE)
-
-    return process.stdout.decode().strip().split('\n')
+    output=process.stdout.decode().strip()
+    
+    if "fatal: bad object" in output:
+        increase_fetch_ct()
+        return get_changed_files(to_parent)
+      
+    return output.split('\n')
 
 
 def current_commit():
