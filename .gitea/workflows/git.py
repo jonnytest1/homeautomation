6e8370c897c2d039f0f96cbf3f5ac7d391845127
jonnytest1 <jonathanheindl@gmx.de>
2025-06-21 10:45:54 +0200

    add more logs

--- .gitea/workflows/git.py
+++ .gitea/workflows/git.py
@@ -3,26 +3,32 @@ import subprocess
 
 
 def increase_fetch_ct():
-    print(subprocess.run(args="pwd", shell=True).stdout)
+    print("increasing fetch ct to 100")
+    print( subprocess.run(args="pwd", shell=True).stdout)
     process = subprocess.run("git fetch --depth=100", shell=True, stdout=subprocess.PIPE)
 
     return process.stdout.decode().strip().split('\n')
 
 
-def get_changed_files(to_parent: str):
+def get_changed_files(to_parent: str,increased_fetch=False):
+    print("get_changed_files")
     print(subprocess.run(args="pwd", shell=True).stdout)
     process = subprocess.run(
         f"git diff --name-only HEAD {to_parent}", shell=True, stdout=subprocess.PIPE)
     output=process.stdout.decode().strip()
     
-    if "fatal: bad object" in output:
+    if "fatal: bad object" in output :
+        print("got bad object")
+        if increased_fetch:
+          raise Exception("already increased fetch")
         increase_fetch_ct()
-        return get_changed_files(to_parent)
+        return get_changed_files(to_parent,True)
       
     return output.split('\n')
 
 
 def current_commit():
+    print("current_commit")
     process = subprocess.run(
         "git rev-parse HEAD", shell=True, stdout=subprocess.PIPE)
 
