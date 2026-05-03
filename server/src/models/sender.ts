224a89b0a6b96b46257cfb02993af3e2d0d1df88
jonnytest1 <jonathanheindl@gmx.de>
2026-05-03 18:13:34 +0200

    add schema option to sender

--- server/src/models/sender.ts
+++ server/src/models/sender.ts
@@ -15,83 +15,89 @@ import { ResponseCodeError } from 'express-hibernate-wrapper';
 @autosaveable
 export class Sender extends Transformer {
 
-    @primary()
-    id: number;
+  @primary()
+  id: number;
 
-    @mapping(Mappings.OneToMany, Connection, 'sender')
-    public connections: Array<Connection> = [];
+  @mapping(Mappings.OneToMany, Connection, 'sender')
+  public connections: Array<Connection> = [];
 
-    @mapping(Mappings.OneToMany, BatteryLevel, 'sender')
-    public batteryEntries: Array<BatteryLevel> = [];
+  @mapping(Mappings.OneToMany, BatteryLevel, 'sender')
+  public batteryEntries: Array<BatteryLevel> = [];
 
-    @mapping(Mappings.OneToMany, EventHistory, 'sender')
-    public events: Array<EventHistory> = [];
+  @mapping(Mappings.OneToMany, EventHistory, 'sender')
+  public events: Array<EventHistory> = [];
 
-    @mapping(Mappings.OneToMany, Transformation, 'sender')
-    transformation: Array<Transformation> = []
+  @mapping(Mappings.OneToMany, Transformation, 'sender')
+  transformation: Array<Transformation> = []
 
-    @column()
-    @settable
-    transformationAttribute;
+  @column()
+  @settable
+  transformationAttribute;
 
-    @column()
-    @settable
-    deviceKey: string;
+  @column()
+  @settable
+  deviceKey: string;
 
-    @column()
-    @settable
-    description: string;
+  @column()
+  @settable
+  description: string;
 
-    @column()
-    @settable
-    name: string;
+  @column()
+  @settable
+  name: string;
 
-    @column()
-    @settable
-    type: "manual" | "automated" = "automated"
 
-    constructor() {
-        super()
+
+  @column({ size: "large" })
+  @settable
+  schema: string;
+
+  @column()
+  @settable
+  type: "manual" | "automated" = "automated"
+
+  constructor() {
+    super()
+  }
+  getTransformer(data: Record<string, unknown>) {
+    return this.transformation.find(transform => transform.transformationKey == data[this.transformationAttribute]);
+  }
+  getContext(data: Record<string, unknown>) {
+    return {
+      ...super.getContext(data),
+      sender: JSON.parse(JSON.stringify(this)),
+      transformer: this.getTransformer(data)
+
     }
-    getTransformer(data: Record<string, unknown>) {
-        return this.transformation.find(transform => transform.transformationKey == data[this.transformationAttribute]);
+  }
+
+
+  async transformData(data: Record<string, unknown>): Promise<{ usedTransformation: Transformation | null; responseData: TransformationRes | false; }> {
+    let newData = data as TransformationRes | false;
+    let usedTransformation: Transformation | null | undefined = null;
+    if (this.transformationAttribute) {
+      usedTransformation = this.getTransformer(data);
+      if (usedTransformation) {
+        newData = await this.transform(data, usedTransformation);
+      } else {
+        logKibana("INFO", {
+          message: "no transformer for message",
+          data: data,
+          transformerAttribute: this.transformationAttribute,
+          keys: this.transformation.map(tr => tr.transformationKey)
+        })
+        newData = false;
+        throw new ResponseCodeError(404, "didnt find tranformation for key")
+      }
+    } else if (this.transformation.length) {
+      usedTransformation = this.transformation[0]
+      newData = await this.transform(data, usedTransformation);
     }
-    getContext(data: Record<string, unknown>) {
-        return {
-            ...super.getContext(data),
-            sender: JSON.parse(JSON.stringify(this)),
-            transformer: this.getTransformer(data)
 
-        }
+    return {
+      usedTransformation,
+      responseData: newData
     }
-
-
-    async transformData(data: Record<string, unknown>): Promise<{ usedTransformation: Transformation | null; responseData: TransformationRes | false; }> {
-        let newData = data as TransformationRes | false;
-        let usedTransformation: Transformation | null | undefined = null;
-        if (this.transformationAttribute) {
-            usedTransformation = this.getTransformer(data);
-            if (usedTransformation) {
-                newData = await this.transform(data, usedTransformation);
-            } else {
-                logKibana("INFO", {
-                    message: "no transformer for message",
-                    data: data,
-                    transformerAttribute: this.transformationAttribute,
-                    keys: this.transformation.map(tr => tr.transformationKey)
-                })
-                newData = false;
-                throw new ResponseCodeError(404, "didnt find tranformation for key")
-            }
-        } else if (this.transformation.length) {
-            usedTransformation = this.transformation[0]
-            newData = await this.transform(data, usedTransformation);
-        }
-
-        return {
-            usedTransformation,
-            responseData: newData
-        }
-    }
+  }
 
 }
